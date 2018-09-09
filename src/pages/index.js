4ee3f6299acf6d87d2c7cdba0c2a68f1e2403e57
Lucas Van Remoortere <lucasvanremoortere@MBP-van-Lucas.lan>
2018-09-09 22:58:56 +0200

    fixed react-icons library build issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,24 @@
 import React from "react";
-import { MdSearch } from 'react-icons/md';
-import { MdCreate } from 'react-icons/md';
-import { MdAvTimer } from 'react-icons/md';
-import { MdDevices } from 'react-icons/md';
+import pencil from "../assets/img/baseline-create-24px.svg";
+import speed from "../assets/img/baseline-av_timer-24px.svg";
+import devices from "../assets/img/baseline-devices-24px.svg";
+import search from "../assets/img/baseline-search-24px.svg";
 
 const iconStyle = {
-  margin: "0 10px"
+  margin: "0 10px",
+  fillColor: "red"
 }
-const IndexPage = () => (
 
+const IndexPage = () => (
   <div>
     <h1>Welkom op de restaurant demo-site</h1>
     <p>Deze website heeft volgende eigenschappen:</p>
     <ul style={{ listStyleType: "none" }}>
-      <li><MdDevices style={iconStyle} /> Responsive design (ontwerp werd aangepast zodat de website er op alle apparaten goed uitziet)</li>
-      <li><MdCreate style={iconStyle} /> Menu-kaart kan aangepast worden a.d.h.v. een erg gebruiksvriendelijk systeem</li>
-      <li><MdAvTimer style={iconStyle} /> Super snelle laadtijd</li>
-      <li><MdSearch style={iconStyle} /> SEO-vriendelijk</li>
+      <li><img src={devices} style={iconStyle} alt="devices" /> Responsive design (ontwerp werd aangepast zodat de website er op alle apparaten goed uitziet)</li>
+      <li><img src={pencil} style={iconStyle} alt="pencil" /> Menu-kaart kan aangepast worden a.d.h.v. een erg gebruiksvriendelijk systeem</li>
+      <li><img src={speed} style={iconStyle} alt="speed" /> Super snelle laadtijd</li>
+      <li><img src={search} style={iconStyle} alt="search" /> SEO-vriendelijk</li>
+      {/* <li><img src={penicl} alt="pencil" /></li> */}
     </ul>
   </div>
 );
