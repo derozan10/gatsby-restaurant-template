a60037eeb5f73baf4f8f23b92b106b3c1777241c
Lucas Van Remoortere <lucas.vanremoortere@gmail.com>
2019-03-13 14:58:46 +0100

    Update Map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -8,10 +8,6 @@ const StyledMap = styled.div`
   width: 100%;
   margin: 1.45rem auto;
   border-radius: 10px;
-  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
-  @media (max-width: 576px) {
-    width: 80vw;
-  }
 `;
 const RestaurantMapLogo = () => (
   <img src={Logo} style={{ width: "70px", height: "auto" }} alt="restaurant logo" />
